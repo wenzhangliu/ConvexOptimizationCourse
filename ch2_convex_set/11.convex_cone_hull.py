fba1d949e2ce1520fa3830954e1e5818f2eda9d8
wenzhangliu <liu_wzh@foxmail.com>
2025-03-05 13:41:56 +0800

    update

--- ch2_convex_set/11.convex_cone_hull.py
+++ ch2_convex_set/11.convex_cone_hull.py
@@ -10,8 +10,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
-from matplotlib.widgets import Button
-from scipy.spatial import ConvexHull
 plt.rcParams['font.family'] = 'Heiti TC'  # 设置中文字体
 
 # 初始化若干个点
@@ -37,7 +35,6 @@ plt.text(points[1, 0], points[1, 1]-0.3, r'$x_2$', fontsize=12)
 plt.text(points[2, 0], points[2, 1]-0.3, r'$x_3$', fontsize=12)
 plt.text(points[3, 0], points[3, 1]-0.3, r'$x_4$', fontsize=12)
 
-# 并绘制三角形平面
 t_max = 5  # 控制射线长度
 origin = np.array([0, 0])
 end_1 = t_max * all_points[max_index]
@@ -48,7 +45,7 @@ ax.add_patch(polygon_surface)
 # 绘制可拖动的点
 x_move, = ax.plot(x[0], x[1], 'ro', markersize=6, label="可拖动点")
 
-# **拖动顶点的类**
+# 拖动点的类
 class DraggablePoint:
     def __init__(self, point_plot, polygon_surface):
         self.point_plot = point_plot
@@ -77,8 +74,6 @@ class DraggablePoint:
 
         # 更新点的新位置
         self.point_plot.set_data(event.xdata, event.ydata)
-
-        # **更新三角形的第三个点**
         new_point = np.array([event.xdata, event.ydata])
         new_sets = np.concatenate([points, new_point.reshape(1, -1)], axis=0)
         new_theta = np.tanh(new_sets[:, 1] / new_sets[:, 0])  # 计算角度
@@ -87,13 +82,11 @@ class DraggablePoint:
         end_1 = t_max * new_sets[max_index]
         end_2 = t_max * new_sets[min_index]
         self.polygon_surface.set_xy([origin, end_1, end_2])
-        # self.triangle_patch.set_xy(convex_hull_points)
-        # self.polygon_surface = Polygon(convex_hull_points, closed=True, edgecolor='b', facecolor='lightblue', alpha=0.6)
 
         # 刷新画布
         self.point_plot.figure.canvas.draw()
 
 
-# **创建可拖动点对象**
+# 创建可拖动点对象
 draggable_point = DraggablePoint(x_move, polygon_surface)
 plt.show()
